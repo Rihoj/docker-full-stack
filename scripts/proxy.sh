c81aeb261ac6896ea5bdca7406d266d0bc93572e
James Ray <james@rayprogramming.com>
2018-11-09 01:12:20 -0500

    Fix dns issues

--- scripts/proxy.sh
+++ scripts/proxy.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 for file in /etc/nginx/conf.d.templates/*; do
     filename=`echo $file | awk -F"/" '{print $NF}'`;
-    # envsubst "`env | awk -F = '{printf \" $$%s\", $$1}'`" < $PWD/conf/proxy/$filename > $PWD/etc/proxy/$filename;
     envsubst "`env | awk -F = '{printf \" $$%s\", $$1}'`" < /etc/nginx/conf.d.templates/$filename > /etc/nginx/conf.d/$filename;
 done;
 nginx -g 'daemon off;'
